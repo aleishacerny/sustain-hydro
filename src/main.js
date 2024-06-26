2b13b08fa92b1d240f798c4f039cbff950dab3b7
fenyanglyy <evelynliu1311@gmail.com>
2024-06-25 22:12:50 -0400

    addViews

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-import Vue from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // Adjust the path if needed
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).use(router).mount('#app');
---
+++ src/views/ContactUsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>contact us Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactUsView',
+};
+</script>
+
+<style scoped>
+/* Add styles if needed */
+</style>
