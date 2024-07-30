2b1d47e575cef392131fd7dfde95ae31889716f8
evelyn <evelynliu1311@gmail.com>
2024-07-29 22:38:05 -0400

    technologypage

---
+++ src/views/CompanyView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>company Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CompanyView',
+};
+</script>
+
+<style scoped>
+/* Add styles if needed */
+</style>
