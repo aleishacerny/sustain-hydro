ae0dbdbfa1e07156b805df6d5a7d52d2de1096db
evelyn <evelynliu1311@gmail.com>
2024-08-05 15:27:45 -0400

    hashurl

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue'; // Replace with actual paths
 import TechnologyView from '@/views/TechnologyView.vue'; // Replace with actual paths
 import ContactUsView from '@/views/ContactUsView.vue'; // Replace with actual paths
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/sustain-hydro/' : '/'),
+  history: createWebHashHistory(process.env.NODE_ENV === 'production' ? '/sustain-hydro/' : '/'),
   routes,
 });
 
