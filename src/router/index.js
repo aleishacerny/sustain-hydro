7bef8901314bde76d4eb89e89032b97ec05f45e9
evelyn <evelynliu1311@gmail.com>
2024-08-07 18:46:45 -0400

    update route

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue'; // Replace with actual paths
 import TechnologyView from '@/views/TechnologyView.vue'; // Replace with actual paths
 import ContactUsView from '@/views/ContactUsView.vue'; // Replace with actual paths
@@ -17,7 +17,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
